343d6010c68770c63f4f5e3965107dab355943e0
lincoln.dias <lincoln.dias@somospi.com.br>
2021-04-14 12:01:05 -0300

    fix: correcão das rotas de postInfluencer

--- src/routes/postRoutes/index.js
+++ src/routes/postRoutes/index.js
@@ -6,8 +6,10 @@ const postController = require("../../controllers/postController")
 routes.post('/post/createPost', postController.createPost)
 
 routes.get('/post', postController.getPost)
-routes.get('/post/:user_id', postController.getPostByUserId)
 routes.get('/post/myPosts', authenticationn.verifyToken, postController.getMyPosts)
+
+routes.get('/post/:postInfluencer', postController.getPostInfluencer)
+routes.get('/post/:user_id', postController.getPostByUserId)
 
 routes.delete('/post/:post_id', postController.deletePost)
 

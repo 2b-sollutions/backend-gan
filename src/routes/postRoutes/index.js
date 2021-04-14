cd5096afaeac59b1884ad139081b99f40da88c0c
lincoln.dias <lincoln.dias@somospi.com.br>
2021-04-14 12:30:41 -0300

    fix: swaggfer including post by user Id

--- src/routes/postRoutes/index.js
+++ src/routes/postRoutes/index.js
@@ -8,7 +8,7 @@ routes.post('/post/createPost', postController.createPost)
 routes.get('/post', postController.getPost)
 routes.get('/post/myPosts', authenticationn.verifyToken, postController.getMyPosts)
 
-routes.get('/post/:postInfluencer', postController.getPostInfluencer)
+routes.get('/post/postInfluencer', postController.getPostInfluencer)
 routes.get('/post/:user_id', postController.getPostByUserId)
 
 routes.delete('/post/:post_id', postController.deletePost)

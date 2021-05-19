137846239ee69a5f8d4fe7d21bde19116021023b
lincoln.dias <lincoln.dias@somospi.com.br>
2021-05-19 14:29:43 -0300

    myCart

--- src/routes/api/v1/cartRoutes/index.js
+++ src/routes/api/v1/cartRoutes/index.js
@@ -5,7 +5,7 @@ const cartController = require('../../../../controllers/cartController')
 
 routes.post('/', authenticationn.verifyToken, cartController.createCart)
 routes.get('/:cartId', cartController.getCartById)
-routes.get('/mycart', authenticationn.verifyToken, cartController.getMyCart)
+routes.post('/mycart', authenticationn.verifyToken, cartController.getMyCart)
 routes.put('/addProduct', authenticationn.verifyToken, cartController.addProduct)
 routes.delete('/removeProduct', authenticationn.verifyToken, cartController.removeProduct)
 

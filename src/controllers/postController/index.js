0c70372af0b763a386f5153bcf68dd7cfe39e045
lincoln.dias <lincoln.dias@somospi.com.br>
2021-06-14 17:48:08 -0300

    correction

--- src/controllers/postController/index.js
+++ src/controllers/postController/index.js
@@ -147,12 +147,47 @@ module.exports = {
   //         return res.status(400).json(error)
   //     }
   // },
+  // async getPostInfluencer (req, res) {
+  //   try {
+  //     const resultEnd = await postServices.getPostInfluencer(req, res)
+  //     return res.status(200).json(resultEnd)
+  //   } catch (error) {
+  //     return res.status(400).json(error.message)
+  //   }
+  // }
   async getPostInfluencer (req, res) {
-    try {
-      const resultEnd = await postServices.getPostInfluencer(req, res)
-      return res.status(200).json(resultEnd)
-    } catch (error) {
-      return res.status(400).json(error.message)
-    }
+    const { page = 1, limit = 10 } = req.query
+    const influencers = await Influencer.find().limit(limit * 1).skip((page - 1) * limit)
+    const payloadResponse = await Promise.all(
+      influencers.map(async (element) => {
+        const posts = await Post.find({ userId: element.userId }).limit(limit * 1).skip((page - 1) * limit)
+        const user = await User.find({ _id: element.userId })
+
+        const postsList = []
+        for (const post of posts) {
+          const productDetailList = await Promise.all(await post.productList.map(async (element) => {
+            const product = await Product.findById({ _id: element })
+
+            return product
+          }))
+          const payloadResponseFor = {
+            postId: post._id,
+            imagePostList: post.imagePostList,
+            descriptionPost: post.description,
+            createdAt: post.createdAt,
+            productDetailList
+          }
+          postsList.push(payloadResponseFor)
+        }
+
+        const payloadResponseEnd = {
+          influencerName: user[0].userName,
+          postsList
+        }
+        return payloadResponseEnd
+      })
+    )
+
+    return payloadResponse
   }
 }
